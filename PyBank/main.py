c5b063dbcb8c47aee9b8f6f54aaabb351e976da2
David Marcus <dmarcus9@hotmail.com>
2018-08-04 09:50:52 -0500

    updated HW

---
+++ PyBank/main.py
@@ -0,0 +1,10 @@
+import os
+print(os.listdir('Resources/'))
+
+file_path = os.path.join('Resources','budget_data.csv')
+print(file_path)
+
+with open(file_path) as file:    
+    
+import csv
+import inspect
