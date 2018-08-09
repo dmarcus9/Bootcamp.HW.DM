271342091b53e5c0f140d2c81f735a1f0b46b969
David Marcus <dmarcus9@hotmail.com>
2018-08-08 21:06:37 -0500

    final for now

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,10 +1,62 @@
 import os
-print(os.listdir('Resources/'))
+import csv
 
 file_path = os.path.join('Resources','budget_data.csv')
-print(file_path)
 
-with open(file_path) as file:    
+total_months = 0
+total_net = 0
+changes = []
+months = []
+previous_profit = 0
+average_change = 0
+greatest_profit_increase = 0
+greatest_loss_decrease = 0
+
+with open(file_path) as file:
+    data = csv.reader(file,delimiter=',')
+    next(data)
+    for row in data:
+        x = float(row[1]) 
+        total_months +=1
+        total_net += x
+        
+        changes.append(x - previous_profit) 
+        
+        months.append(row[0]) 
+        previous_profit = x
     
-import csv
-import inspect
+    greatest_profit_increase = max(changes) 
+    greatest_loss_decrease = min(changes)
+    
+    changes.pop(0) 
+    
+    average_change = sum(changes) / len(changes)
+        
+    increase_index = changes.index(greatest_profit_increase)+1
+    decrease_index = changes.index(greatest_loss_decrease)+1
+    
+    average_change = str(round(average_change, 2))
+    
+    total_net = '{:,}'.format(total_net)
+    greatest_profit_increase = '{:,}'.format(greatest_profit_increase)
+    greatest_loss_decrease = '{:,}'.format(greatest_loss_decrease)
+    
+print ("Total Months:", total_months)
+print ("Total Net Amount: $", total_net)
+print ("Average Change: $", average_change)
+print ("Greatest Increase in Profits:", months[increase_index], greatest_profit_increase)
+print ("Greatest Decrease in Losses:", months[decrease_index], greatest_loss_decrease)
+
+output = ('Total Months: {}\n Total Net Amount: $ {} \n Average Change: $ {} \n Greatest Increase in Profits: {} \n Greatest Decrease in Losses: {}').format(total_months, total_net, average_change, (months[increase_index], greatest_profit_increase), (months[decrease_index], greatest_loss_decrease))  
+
+#In addition, your final script should both 
+#print the analysis to the terminal and 
+#export a text file with the results.
+
+# Specify the file to write to
+output_path = os.path.join("Resources", "output.txt")
+
+# Open the file using "write" mode. Specify the variable to hold the contents
+with open(output_path, 'w', newline='') as txtfile:
+    
+    txtfile.write(output)
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,3 +1,80 @@
 import os
+import csv
 
-set path...
+file_path = os.path.join('Resources','election_data.csv')
+
+#The total number of votes cast
+#A complete list of candidates who received votes
+#The percentage of votes each candidate won
+#The total number of votes each candidate won
+
+total_votes = 0
+Khan_votes = []
+Correy_votes = []
+Li_votes = []
+OTooley_votes = []
+Winning_votes = 0
+Winner = ""
+
+with open(file_path) as file:
+    data = csv.reader(file,delimiter=',')
+    next(data)
+    for row in data:
+        x = str(row[2]) 
+        total_votes +=1
+        
+        if row[2] == "Khan":
+            Khan_votes.append("Khan")
+        elif row[2] == "Correy":
+            Correy_votes.append("Correy")
+        elif row[2] == "Li":
+            Li_votes.append("Li")   
+        elif row[2] == "O'Tooley":
+             OTooley_votes.append("O'Tooley") 
+    
+if len(Khan_votes) > Winning_votes:
+    Winning_votes = len(Khan_votes)
+    Winner = "Khan"
+if len(Correy_votes) > Winning_votes:
+    Winning_votes = len(Correy_votes)
+    Winner = "Correy"
+if len(Li_votes) > Winning_votes:
+    Winning_votes = len(Li_votes)
+    Winner = "Li"
+if len(OTooley_votes) > Winning_votes:
+    Winning_votes = len(OTooley_votes)
+    Winner = "O'Tooley"   
+    
+# '{:.2f}'.format shortened the percentages to 2 decimal places
+# and I made a variable to hold the values
+Khan_percent = '{:.2f}'.format(len(Khan_votes)/total_votes*100)
+Correy_percent = '{:.2f}'.format(len(Correy_votes)/total_votes*100)
+Li_percent = '{:.2f}'.format(len(Li_votes)/total_votes*100)
+OTooley_percent = '{:.2f}'.format(len(OTooley_votes)/total_votes*100)
+
+total_votes = '{:,}'.format(total_votes)
+
+print ("Election Results") 
+print ("----------------------") 
+print ("Total Votes:", total_votes)
+print ("----------------------")  
+
+print ("Khan: " + Khan_percent + "%  " + str(len(Khan_votes)))
+print ("Correy: " + Correy_percent + "%  " + str(len(Correy_votes)))
+print ("Li: " + Li_percent + "%  " + str(len(Li_votes))) 
+print ("O'Tooley: " + OTooley_percent + "%  " + str(len(OTooley_votes)))
+print ("----------------------") 
+print ("Winner: " + Winner)
+
+with open("Resources/output.txt", "w") as text_file:
+    text_file.write("Election Results\n")
+    text_file.write("----------------------\n") 
+    text_file.write("Total Votes: " + str(total_votes)+ "\n")
+    text_file.write("----------------------\n") 
+    text_file.write("Khan: " + str(Khan_percent) + "%  " + str(len(Khan_votes))+ "\n") 
+    text_file.write("Correy: " + str(Correy_percent) + "%  " + str(len(Correy_votes))+ "\n")
+    text_file.write("Li: " + str(Li_percent) + "%  " + str(len(Li_votes))+ "\n") 
+    text_file.write("O'Tooley: " + str(OTooley_percent) + "%  " + str(len(OTooley_votes))+ "\n")
+    text_file.write("----------------------\n") 
+    text_file.write("Winner: " + Winner) 
+
